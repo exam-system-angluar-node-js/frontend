95b574013b547148e4aa877769d04f094709336b
Mohamed Elgendy <93053571+gendyvip@users.noreply.github.com>
2025-05-03 21:54:10 +0200

    Add Login Validation

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required,
+      Validators.pattern(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/)]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
 
+  get getEmail() {
+    return this.loginForm.get('email');
+  }
+  get getPassword() {
+    return this.loginForm.get('password');
+  }
+
+  login() {
+    if (this.loginForm.status === 'VALID') {
+        console.log(this.loginForm.value);
+    } else {
+      //make the @if in html work
+      this.loginForm.markAllAsTouched();
+      console.log('error');
+    }
+  }
 }
+
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -20,7 +20,8 @@ export class RegisterComponent {
       Validators.minLength(3),
       Validators.pattern(/^[a-zA-Z]+$/),
     ]),
-    email: new FormControl('', [Validators.required, Validators.email]),
+    email: new FormControl('', [Validators.required,
+       Validators.pattern(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/)]),
     password: new FormControl('', [
       Validators.required,
       Validators.minLength(6),
