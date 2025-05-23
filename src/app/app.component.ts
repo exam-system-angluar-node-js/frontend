563015bbdcf1aba1a21b26ce9abe3b13a384f1f2
Mohamed Elgendy <93053571+gendyvip@users.noreply.github.com>
2025-05-23 23:58:19 +0200

    Latest Features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
+import { initFlowbite } from 'flowbite';
+import { DashboardComponent } from "./pages/dashboard/dashboard.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RegisterComponent,LoginComponent],
+  imports: [RouterOutlet, RegisterComponent, LoginComponent, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { ResultsComponent } from './pages/results/results.component';
+import { ExamsComponent } from './pages/exams/exams.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: RegisterComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'settings', component: SettingsComponent },
+  { path: 'results', component: ResultsComponent },
+  { path: 'exams', component: ExamsComponent },
 ];
