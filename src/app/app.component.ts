d1264739f8de3f4b7a6d2404a0b970690cff1e29
Mohamed Ashraf <moa412003@gmail.com>
2025-05-24 00:14:42 +0300

    make shared card and enhance login & register

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
+import { initFlowbite } from 'flowbite';
+import { CardComponent } from './shared/components/card/card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RegisterComponent,LoginComponent],
+  imports: [RouterOutlet, RegisterComponent, LoginComponent, CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
