0fe3e7e12c8b96d94382cd4fed73691e5cd8319e
Mohamed Elgendy <93053571+gendyvip@users.noreply.github.com>
2025-05-27 04:03:45 +0200

    Adding data service & designing the whole app

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+    private dataSource = new BehaviorSubject<any[]>([]);
+    currentData = this.dataSource.asObservable();
+  changeData(data: any[]) {
+    this.dataSource.next(data);
+  }
+}
